bcc15de6cc3239b892c7ea87b33df5e7e7bbe941
KarthickSakthi <karthicksakthi124@gmail.com>
2021-06-27 11:02:34 +0530

    Random List color view commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,100 +1,89 @@
+import 'dart:math';
+
 import 'package:flutter/material.dart';
-import 'package:flutter_staggered_grid_view/flutter_staggered_grid_view.dart';
-import 'package:flutter_svg/svg.dart';
-import 'package:transparent_image/transparent_image.dart';
+
 
 void main() {
   runApp(MaterialApp(
     debugShowCheckedModeBanner: false,
-    home: StaggeredGridCategory(),
+    home: RandomListColorPage(),
   ));
 }
 
-class StaggeredGridCategory extends StatefulWidget {
+class RandomListColorPage extends StatefulWidget {
   // This widget is the root of your application.
   @override
-  _StaggeredGridCategoryState createState() => _StaggeredGridCategoryState();
+  _RandomListColorPageState createState() => _RandomListColorPageState();
 }
 
-class _StaggeredGridCategoryState extends State<StaggeredGridCategory> {
-  List<String> imageList = [
-    'https://cdn.pixabay.com/photo/2020/12/09/09/27/women-5816861_960_720.jpg',
-    'https://cdn.pixabay.com/photo/2020/12/15/16/25/clock-5834193__340.jpg',
-    'https://cdn.pixabay.com/photo/2020/09/18/19/31/laptop-5582775_960_720.jpg',
-    'https://media.istockphoto.com/photos/woman-kayaking-in-fjord-in-norway-picture-id1059380230?b=1&k=6&m=1059380230&s=170667a&w=0&h=kA_A_XrhZJjw2bo5jIJ7089-VktFK0h0I4OWDqaac0c=',
-    'https://cdn.pixabay.com/photo/2019/11/05/00/53/cellular-4602489_960_720.jpg',
-    'https://cdn.pixabay.com/photo/2017/02/12/10/29/christmas-2059698_960_720.jpg',
-    'https://cdn.pixabay.com/photo/2020/01/29/17/09/snowboard-4803050_960_720.jpg',
-    'https://cdn.pixabay.com/photo/2020/02/06/20/01/university-library-4825366_960_720.jpg',
-    'https://cdn.pixabay.com/photo/2020/11/22/17/28/cat-5767334_960_720.jpg',
-    'https://cdn.pixabay.com/photo/2020/12/13/16/22/snow-5828736_960_720.jpg',
-    'https://cdn.pixabay.com/photo/2020/12/09/09/27/women-5816861_960_720.jpg',
-  ];
-
-  @override
+class _RandomListColorPageState extends State<RandomListColorPage> {
+ @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.black,
-      body: SafeArea(
-             child: Column(
-                 children: [
-                SizedBox(
-                  height: 30,
-                ),
-                Text(
-                  "Welcome to Gallery",
-                  textAlign: TextAlign.right,
-                  style: TextStyle(
-                      fontSize: 30,
-                      fontWeight: FontWeight.normal,
-                      color: Colors.white),
-                ),
-                SizedBox(
-                  height: 10,
-                ),
-                Text(
-                  "What you're looking for ?",
-                  textAlign: TextAlign.left,
-                  style: TextStyle(
-                      fontSize: 20,
-                      fontWeight: FontWeight.normal,
-                      color: Colors.grey),
-                ),
-                SizedBox(
-                  height: 10,
-                ),
-                Flexible(
-                  child: Container(
-                    margin: EdgeInsets.all(12),
-                    child: StaggeredGridView.countBuilder(
-                        shrinkWrap: true,
-                        crossAxisCount: 2,
-                        crossAxisSpacing: 10,
-                        mainAxisSpacing: 12,
-                        itemCount: imageList.length,
-                        itemBuilder: (context, index) {
-                          return Container(
-                            decoration: BoxDecoration(
-                                color: Colors.transparent,
-                                borderRadius:
-                                    BorderRadius.all(Radius.circular(15))),
-                            child: ClipRRect(
-                              borderRadius: BorderRadius.all(Radius.circular(15)),
-                              child: FadeInImage.memoryNetwork(
-                                placeholder: kTransparentImage,
-                                image: imageList[index],
-                                fit: BoxFit.cover,
+        appBar: AppBar(
+          backgroundColor: Colors.black,
+          centerTitle: true,
+          title: Text("Offers"),
+        ),
+        body: ListView.builder(
+          itemBuilder: (context, index) {
+            return Padding(
+              padding: EdgeInsets.symmetric(vertical: 3.0, horizontal: 8.0),
+              child: Card(
+                color:
+                Colors.primaries[Random().nextInt(Colors.primaries.length)],
+                shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(20.0)),
+                child: SizedBox(
+                  width: double.infinity,
+                  height: 80.0,
+                  child: Padding(
+                    padding: const EdgeInsets.all(15.0),
+                    child: Row(
+                      children: [
+                        Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: [
+                            Text(
+                              "30% off",
+                              style: Theme.of(context)
+                                  .textTheme
+                                  .subtitle1
+                                  .copyWith(
+                                fontWeight: FontWeight.bold,
+                                color: Colors.white,
                               ),
                             ),
-                          );
-                        },
-                        staggeredTileBuilder: (index) {
-                          return StaggeredTile.count(1, index.isEven ? 1.2 : 1.8);
-                        }),
+                            Text(
+                              "Best deals of Today",
+                              style: Theme.of(context)
+                                  .textTheme
+                                  .subtitle1
+                                  .copyWith(
+                                fontWeight: FontWeight.bold,
+                                color: Colors.white,
+                              ),
+                            ),
+                          ],
+                        ),
+                        Spacer(),
+                        Icon(
+                          Icons.local_offer,
+                          color: Colors.white,
+                        ),
+                      ],
+                    ),
                   ),
                 ),
-              ]),
-           ),
-        );
-  }
+              ),
+            );
+          },
+          itemCount: 10,
+        )
+      );
+ }
 }
+
+
