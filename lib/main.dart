c6022ced3741be5bcc6383b832612b4b4aad2f61
Suganth S <suganthsece86@gmail.com>
2021-07-11 08:48:51 +0530

    Grid view commit

--- lib/main.dart
+++ lib/main.dart
@@ -5,73 +5,24 @@ import 'package:flutter/material.dart';
 void main() {
   runApp(MaterialApp(
     debugShowCheckedModeBanner: false,
-    home: RandomListColorPage(),
+    home: GridViewPage(),
   ));
 }
 
-class RandomListColorPage extends StatefulWidget {
+class GridViewPage extends StatefulWidget {
   // This widget is the root of your application.
   @override
-  _RandomListColorPageState createState() => _RandomListColorPageState();
+  _GridViewPageState createState() => _GridViewPageState();
 }
 
-class _RandomListColorPageState extends State<RandomListColorPage> {
+class _GridViewPageState extends State<GridViewPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.black,
       appBar: AppBar(
-        backgroundColor: Colors.black,
-        centerTitle: true,
-        title: Text("Offers"),
-      ),
-      body: ListView.builder(itemBuilder: (context, index) {
-        return Padding(
-          padding: EdgeInsets.symmetric(vertical: 3.0, horizontal: 8.0),
-          child: Card(
-            color: Colors.primaries[Random().nextInt(Colors.primaries.length)],
-            shape: RoundedRectangleBorder(
-              borderRadius: BorderRadius.circular(20.0),
-            ),
-            child: SizedBox(
-              width: double.infinity,
-              height: 80.0,
-              child: Padding(
-                padding: const EdgeInsets.all(15.0),
-                child: Row(
-                  children: [
-                    Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      children: [
-                        Text(
-                          "30% off",
-                          style: Theme.of(context).textTheme.subtitle1.copyWith(
-                              fontWeight: FontWeight.bold, color: Colors.white),
-                        ),
-                        Text(
-                          "BesT Deals of Today",
-                          style: Theme.of(context).textTheme.subtitle1.copyWith(
-                                fontWeight: FontWeight.bold,
-                                color: Colors.white,
-                              ),
-                        ),
-                      ],
-                    ),
-                    Spacer(),
-                    Icon(
-                      Icons.local_offer,
-                      color:Colors.white,
-                    ),
-                  ],
-                ),
-              ),
-            ),
-          ),
-        );
-      },
-      itemCount: 10,
-      ),
-    );
+        backgroundColor: Colors.black87,
+        centerTitle: true ,
+          title: Text("Grid View"),
+      ));
   }
 }
