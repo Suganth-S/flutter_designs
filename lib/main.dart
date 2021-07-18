1fd35a814565aacec2e66e8d71c7b8a5ff7a6e80
Suganth S <suganthsece86@gmail.com>
2021-07-18 17:08:56 +0530

    Horizontal List view commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,63 +1,93 @@
 import 'dart:math';
 
 import 'package:flutter/material.dart';
-import 'package:transparent_image/transparent_image.dart';
 
 void main() {
   runApp(MaterialApp(
     debugShowCheckedModeBanner: false,
-    home: GridViewPage(),
+    home: HorizontalListPage(),
   ));
 }
 
-class GridViewPage extends StatefulWidget {
+class HorizontalListPage extends StatefulWidget {
   // This widget is the root of your application.
   @override
-  _GridViewPageState createState() => _GridViewPageState();
+  _HorizontalListPageState createState() => _HorizontalListPageState();
 }
 
-class _GridViewPageState extends State<GridViewPage> {
-  List<String> imageList = [
-    'https://cdn.pixabay.com/photo/2020/12/09/09/27/women-5816861_960_720.jpg',
-    'https://cdn.pixabay.com/photo/2020/12/15/16/25/clock-5834193__340.jpg',
-    'https://cdn.pixabay.com/photo/2020/09/18/19/31/laptop-5582775_960_720.jpg',
-    'https://media.istockphoto.com/photos/woman-kayaking-in-fjord-in-norway-picture-id1059380230?b=1&k=6&m=1059380230&s=170667a&w=0&h=kA_A_XrhZJjw2bo5jIJ7089-VktFK0h0I4OWDqaac0c=',
-    'https://cdn.pixabay.com/photo/2019/11/05/00/53/cellular-4602489_960_720.jpg',
-    'https://cdn.pixabay.com/photo/2017/02/12/10/29/christmas-2059698_960_720.jpg',
-    'https://cdn.pixabay.com/photo/2020/01/29/17/09/snowboard-4803050_960_720.jpg',
-    'https://cdn.pixabay.com/photo/2020/02/06/20/01/university-library-4825366_960_720.jpg',
-    'https://cdn.pixabay.com/photo/2020/11/22/17/28/cat-5767334_960_720.jpg',
-    'https://cdn.pixabay.com/photo/2020/12/13/16/22/snow-5828736_960_720.jpg',
-
+class _HorizontalListPageState extends State<HorizontalListPage> {
+  List<Map> categories = [
+    {'name': 'All', 'iconPath': 'assets/icons/all_icon.png'},
+    {'name': 'My', 'iconPath': 'assets/icons/fav_icon.png'},
+    {'name': 'Anxious', 'iconPath': 'assets/icons/anxious_icon.png'},
+    {'name': 'Kids', 'iconPath': 'assets/icons/kid_icon.png'},
+    {'name': 'Sleep', 'iconPath': 'assets/icons/moon_icon.png'},
+    {'name': 'All', 'iconPath': 'assets/icons/all_icon.png'},
+    {'name': 'My', 'iconPath': 'assets/icons/fav_icon.png'},
+    {'name': 'Anxious', 'iconPath': 'assets/icons/anxious_icon.png'},
+    {'name': 'Kids', 'iconPath': 'assets/icons/kid_icon.png'},
+    {'name': 'Sleep', 'iconPath': 'assets/icons/moon_icon.png'},
   ];
+
+  Color primaryBlue = Color(0xFF586894);
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.black,
-      appBar: AppBar(
-        backgroundColor: Colors.black87,
-        centerTitle: true,
-        title: Text("Grid View"),
-      ),
-      body: GridView.builder(
-          shrinkWrap: true,
-          itemCount: imageList.length,
-          gridDelegate:
-              SliverGridDelegateWithFixedCrossAxisCount(crossAxisCount: 2),
-          itemBuilder: (context, index) {
-            return Padding(
-              padding: const EdgeInsets.all(12.0),
-              child: ClipRRect(
-                borderRadius: BorderRadius.all(Radius.circular(15)),
-                child: FadeInImage.memoryNetwork(
-                  placeholder: kTransparentImage,
-                  image: imageList[index],
-                  fit: BoxFit.cover,
-                ),
+        backgroundColor: Colors.black,
+        body: SingleChildScrollView(
+          child: Column(children: [
+            SizedBox(
+              height: 250,
+            ),
+            Text(
+              "Categories",
+              style: TextStyle(
+                fontSize: 40.0,
+                fontWeight: FontWeight.bold,
+                fontStyle: FontStyle.italic,
+                color: Colors.white
               ),
-            );
-          }),
-    );
+                textAlign: TextAlign.center,
+            ),
+            SizedBox(
+              height: 40,
+            ),
+            Container(
+              height: 150,
+              child: ListView.builder(
+                  scrollDirection: Axis.horizontal,
+                  itemCount: categories.length,
+                  itemBuilder: (context, index) {
+                    return Container(
+                      child: Column(
+                        children: [
+                          Container(
+                            padding: EdgeInsets.all(10),
+                            margin: EdgeInsets.only(left: 20),
+                            decoration: BoxDecoration(
+                                color: Colors.deepOrangeAccent,
+                                borderRadius: BorderRadius.circular(10)),
+                            child: Image.asset(
+                              categories[index]['iconPath'],
+                              height: 100,
+                              width: 100,
+                            ),
+                          ),
+                          SizedBox(
+                            height: 10,
+                          ),
+                          Text(
+                            categories[index]['name'],
+                            style: TextStyle(color: Colors.white),
+                            textAlign: TextAlign.center,
+                          )
+                        ],
+                      ),
+                    );
+                  }),
+            ),
+          ]),
+        ));
   }
 }
